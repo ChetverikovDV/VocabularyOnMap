56304675f8e03ccddc7913cb42c1dbfa6e8f4963
DMITRY CHETVERIKOV <76950379+ChetverikovDV@users.noreply.github.com>
2022-03-01 01:37:59 +0700

    упихал создание словаря в Actions тоже

--- src/MapLesson/Actions.java
+++ src/MapLesson/Actions.java
@@ -1,10 +1,9 @@
 package MapLesson;
 //поиск ключа по значению
-
 import java.util.HashMap;
 
 public class Actions {
-    String findKey(HashMap<String, String> map, String key) {
+   public String findKey(HashMap<String, String> map, String key) {
         String engKey = "";
         for (String k : map.keySet()) {
             if (map.get(k).equals(key)) {
@@ -13,5 +12,16 @@ public class Actions {
             }
         }
         return engKey;
+    }
+    public HashMap<String, String> generateMap(){
+        HashMap<String, String> map = new HashMap();
+        map.put("hat", "шляпа");
+        map.put("ball", "мяч");
+        map.put("sword", "меч");
+        map.put("ant", "муравей");
+        map.put("exit", "выход");
+        map.put("стол", "table");
+        map.put("table", "стол");
+        return map;
     }
 }
--- src/MapLesson/Main.java
+++ src/MapLesson/Main.java
@@ -3,14 +3,9 @@ import java.util.*;
 
 public class Main {
     public static  void main(String[] args){
-        HashMap<String, String> map = new HashMap<>();
-        map.put("hat", "шляпа");
-        map.put("ball", "мяч");
-        map.put("sword", "меч");
-        map.put("ant", "муравей");
-        map.put("exit", "выход");
-        map.put("стол", "table");
-        map.put("table", "стол");
+
+        Actions generate = new Actions();
+        HashMap<String, String> map = generate.generateMap();
 
         Scanner scanner = new Scanner(System.in);
 
